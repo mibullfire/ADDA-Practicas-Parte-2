b358041cc6b8cbfe518f4db3d2ef626669e59ad9
Miguel Toro Llamas <mibullfire@gmail.com>
2026-04-08 18:16:55 +0200

    Base PI4

--- PI3_2526_PBase/src/ejercicio4/Solucion4.java
+++ PI3_2526_PBase/src/ejercicio4/Solucion4.java
@@ -47,43 +47,43 @@ public class Solucion4 {
 	}
 
 	private Solucion4(List<Integer> ls) {
-    	txt = "";
-        List<Integer> aux = List2.addLast(ls, ls.getFirst());
-        
-    	camino = aux.stream().map(i -> Datos4.getVertex(i)+"")
-        	.collect(Collectors.joining(" ->\n\t", "Recorrido:\n\t", ""));
-        
-    	// Tiempo total empleado:
-    	totalTime = IntStream.range(0, Datos4.N)
-    		    .mapToDouble(i -> Datos4.tiempo(ls.get(i), ls.get((i + 1) % Datos4.N)))
-    		    .sum();
-        /// Cocinando con Miguel: el (x+1)%Datos4.N) sale de que tenemos que hacerle el módulo para tener el último elemento
-        /// con el primero elemento de la lista cromosómica.
-        
-        avgTime = totalTime / Datos4.N;
-        
-        // Esfuerzo total empleado:
-        totalEffort = IntStream.range(0, Datos4.N)
-        	    .mapToDouble(i -> Datos4.esfuerzo(ls.get(i), ls.get((i + 1) % Datos4.N)))
-        	    .sum();
-        avgEffort = totalEffort / Datos4.N;
-        
-        // Número de lugares con monumento consecutivos a otro monumento:
-        monCons = (double) IntStream.range(0, Datos4.N)
-        	    .filter(i -> {
-        	        int actual = ls.get(i);
-        	        
-        	        // Calculamos quién es el anterior (sumamos N antes del módulo para evitar negativos)
-        	        int anterior = ls.get((i - 1 + Datos4.N) % Datos4.N); 
-        	        // Calculamos quién es el siguiente
-        	        int siguiente = ls.get((i + 1) % Datos4.N);           
-        	        
-        	        // El lugar cuenta si forma pareja de monumentos con el de atrás O con el de adelante
-        	        return Datos4.sonMonumentos(anterior, actual) || Datos4.sonMonumentos(actual, siguiente);
-        	    })
-        	    .count();
-        
-        //monCons = (double) IntStream.range(0, Datos4.N);
+	    	txt = "";
+	        List<Integer> aux = List2.addLast(ls, ls.getFirst());
+	        
+	    	camino = aux.stream().map(i -> Datos4.getVertex(i)+"")
+	        	.collect(Collectors.joining(" ->\n\t", "Recorrido:\n\t", ""));
+	        
+	    	// Tiempo total empleado:
+	    	totalTime = IntStream.range(0, Datos4.N)
+	    		    .mapToDouble(i -> Datos4.tiempo(ls.get(i), ls.get((i + 1) % Datos4.N)))
+	    		    .sum();
+	    /// Cocinando con Miguel: el (x+1)%Datos4.N) sale de que tenemos que hacerle el módulo para tener el último elemento
+	    /// con el primero elemento de la lista cromosómica.
+	    
+	    avgTime = totalTime / Datos4.N;
+	    
+	    // Esfuerzo total empleado:
+	    totalEffort = IntStream.range(0, Datos4.N)
+	    	    .mapToDouble(i -> Datos4.esfuerzo(ls.get(i), ls.get((i + 1) % Datos4.N)))
+	    	    .sum();
+	    avgEffort = totalEffort / Datos4.N;
+	    
+	    // Número de lugares con monumento consecutivos a otro monumento:
+	    monCons = (double) IntStream.range(0, Datos4.N)
+	    	    .filter(i -> {
+	    	        int actual = ls.get(i);
+	    	        
+	    	        // Calculamos quién es el anterior (sumamos N antes del módulo para evitar negativos)
+	    	        int anterior = ls.get((i - 1 + Datos4.N) % Datos4.N); 
+	    	        // Calculamos quién es el siguiente
+	    	        int siguiente = ls.get((i + 1) % Datos4.N);           
+	    	        
+	    	        // El lugar cuenta si forma pareja de monumentos con el de atrás O con el de adelante
+	    	        return Datos4.sonMonumentos(anterior, actual) || Datos4.sonMonumentos(actual, siguiente);
+	    	    })
+	    	    .count();
+	    
+	    //monCons = (double) IntStream.range(0, Datos4.N);
      }
 
 	@Override
